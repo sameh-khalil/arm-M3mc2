b28b765f1605db049f76b052347d77cab76e6f24
unknown <samehkh35@gmail.com>
2022-08-21 18:41:22 +0200

    with ctos and blockdiagram

--- Src/Appl/main.c
+++ Src/Appl/main.c
@@ -5,15 +5,22 @@
 #include "IntCtrl.h"
 #include "Port.h"
 #include "SysClkCtrl.h"
+#include "ON_OFF_LED_TIME_CONFIG.h"
+
 uint32 GETTIMEINTICKS (uint32 freq ,uint32 TimeInSeconds , uint32 prescaler);
+static uint32 TimeTicksOn ;
+static uint32 TimeTicksOff ;
+static uint8 flag = 1 ;
+
 int main (){
 	
 	SYSCTRL_Init();
 	IntCtrl_init();
 	Port_Init(ConfiType);
 	Gpt_Init(TimeConfig);
-	uint32 TimeTicks = GETTIMEINTICKS(20000000,5,1);
-	Gpt_StartTimer(1,TimeTicks);
+	TimeTicksOn = GETTIMEINTICKS(20000000,TIME_ON_IN_sec,1);
+	TimeTicksOff = GETTIMEINTICKS(20000000,TIME_OFF_IN_sec,1);
+	Gpt_StartTimer(1,TimeTicksOn);
 	while(1)
 	{}
 }
@@ -27,12 +34,23 @@ uint32 GETTIMEINTICKS (uint32 freq ,uint32 TimeInSeconds , uint32 prescaler)
 
 void TIMER0A_Handler(void)
 {
-	(*TimeConfig[PredegT].GptNotificationPtr)();
+	
 }
 
 void TIMER1A_Handler(void)
 {
-	(*TimeConfig[PredegT].GptNotificationPtr)();
-	GPT_1->GPTMICR=1;
+	if (flag==1)
+		{
+			(*TimeConfig[PredegT].GptNotificationPtr)();
+			Gpt_StartTimer(1,TimeTicksOff);
+			flag = 0 ;
+		}
+		else
+		{
+			(*TimeConfig[PredegT].GptNotificationPtr)();
+			Gpt_StartTimer(1,TimeTicksOn);
+			flag = 1 ;
+		}
+		GPT_1->GPTMICR=1;
 
 }
---
+++ Src/config/ON_OFF_LED_TIME_CONFIG.h
@@ -0,0 +1,12 @@
+#ifndef ON_OFF_TIME_CONFIG_H
+#define ON_OFF_TIME_CONFIG_H
+
+
+
+
+#define		TIME_ON_IN_sec	5
+#define		TIME_OFF_IN_sec 2
+
+
+
+#endif
